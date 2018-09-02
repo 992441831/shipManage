c53f63702958139ddfb58db36858188d0da1c47e
Yecan <Yecan@LAPTOP-4VS1LM5S>
2018-09-02 21:47:45 +0800

    2222222

--- src/main/webapp/Martime/js/data_refresh.js
+++ src/main/webapp/Martime/js/data_refresh.js
@@ -1,15 +1,22 @@
 function shipDataRefresh(){
-	$.ajax({
-		type:'GET',
-		url:"/shipManage/module/shipManage/refreshData.do",
-	    data:{
-	    },
-	    success: function(result)
-	    {
-		  console.log(result);
-	   },
-	   error: function(result) {
-		//alert("发送请求失败:状态码" + result.status);
-	}
-})
+    $.ajax({
+        type:'GET',
+        url:"/shipManage/module/shipManage/refreshData.do",
+        data:{
+        },
+        success: function(result)
+        {
+            console.log(result);
+            var status=result.status;
+            var res = $("#div");
+            if(status==0){
+                alert(result.msg);
+                res.append(JSON.stringify(result.newAddr));
+                res.append("<br>");
+            }
+        },
+        error: function(result) {
+            alert("发送请求失败:状态码" + result.status);
+        }
+    })
 }
--- src/main/webapp/Martime/js/ship_list.js
+++ src/main/webapp/Martime/js/ship_list.js
@@ -6,75 +6,80 @@
 
 function loadShipByPage(pno1,pageSize1,dataStr1){
     dataStr=dataStr1;
-	pageSize=pageSize1;
-	pno=pno1;
-	pattern=1;
-$.ajax({
-    type:"GET",
-    url:"/shipManage/module/shipManage/queryShip.do",
-    data:{dataStr:dataStr,pno:pno,pageSize:pageSize},
-    success:function(data){
-        //*3:动态创建表格中多行 id="tbody1"
-    	//console.log(data);
-        var rows = data.list;
-        var html = "";                 //声明空字符串
-        for(var i=0;i<rows.length;i++) {//循环拼接
-            var obj = rows[i];
-            if(obj.access_port_date=='1900-01-01'){
-            	obj.access_port_date='-';
+    pageSize=pageSize1;
+    pno=pno1;
+    pattern=1;
+    $.ajax({
+        type:"GET",
+        url:"/shipManage/module/shipManage/queryShip.do",
+        data:{dataStr:dataStr,pno:pno,pageSize:pageSize},
+        success:function(data){
+            //*3:动态创建表格中多行 id="tbody1"
+            //console.log(data.list.length);
+            var rows = data.list;
+            var html = "";                 //声明空字符串
+            if(rows.length==0){
+                html="";
+            }else{
+                for(var i=0;i<rows.length;i++) {//循环拼接
+                    var obj = rows[i];
+                    if(obj.access_port_date=='1900-01-01'){
+                        obj.access_port_date='-';
+                    }
+                    if(obj.length==0){
+                        obj.length='-';
+                    }
+                    if(obj.tonnage==0){
+                        obj.tonnage='-';
+                    }
+                    if(obj.weigh_date=='2118-01-01'){
+                        obj.weigh_date='-';
+                    }
+                    html += `
+                  <tr>   
+                                <td>${obj.id}</td>
+                                <td>${obj.name}</td>
+                                <td>${obj.length}</td>
+                                <td>${obj.tonnage}</td>
+                                <td>${obj.anchor_date}</td>
+                                <td>${obj.target_port}</td>
+                                <td>${obj.weigh_date}</td>
+                                <td>${obj.anchor_days}</td>
+                                <td>${obj.access_port_date}</td>
+                                <td>${obj.telephone}</td>
+                                <td>${obj.break_rules}</td>
+                            </tr>`;
+                }//for end
+                $("#tbody1").html(html);       //保存tbody
             }
-            if(obj.length==0){
-            	obj.length='-';
+
+            //5:动态创建分页条 1 2 【3】 4 5 最多5页
+            var html = "";
+            //上上一页
+
+            if(pno-2>0){
+                html += '<li><a href="#">'+(pno-2)+'</a></li>';
             }
-            if(obj.tonnage==0){
-            	obj.tonnage='-';
+            //上一页 10:53--11:03
+            if(pno-1>0){
+                html += '<li><a href="#">'+(pno-1)+'</a></li>';
             }
-            if(obj.weigh_date=='2118-01-01'){
-            	obj.weigh_date='-';
+            //当前页
+            html += '<li class="active"><a href="#">'+pno+'</a></li>';
+            //下一页
+            if(pno+1<=data.pageCount){
+                html += '<li><a href="#">'+(pno+1)+'</a></li>';
             }
-            html += `
-              <tr>   
-                            <td>${obj.id}</td>
-                            <td>${obj.name}</td>
-                            <td>${obj.length}</td>
-                            <td>${obj.tonnage}</td>
-                            <td>${obj.anchor_date}</td>
-                            <td>${obj.target_port}</td>
-                            <td>${obj.weigh_date}</td>
-                            <td>${obj.anchor_days}</td>
-                            <td>${obj.access_port_date}</td>
-                            <td>${obj.telephone}</td>
-                            <td>${obj.break_rules}</td>
-                        </tr>`;
-        }//for end
-        $("#tbody1").html(html);       //保存tbody
-      //5:动态创建分页条 1 2 【3】 4 5 最多5页
-      var html = "";
-      //上上一页
-     
-      if(pno-2>0){
-       html += '<li><a href="#">'+(pno-2)+'</a></li>';
-      }
-      //上一页 10:53--11:03
-      if(pno-1>0){
-      html += '<li><a href="#">'+(pno-1)+'</a></li>';
-      }
-      //当前页
-      html += '<li class="active"><a href="#">'+pno+'</a></li>';
-      //下一页
-      if(pno+1<=data.pageCount){
-      html += '<li><a href="#">'+(pno+1)+'</a></li>';
-      }
-      //下下一页
-      if(pno+2<=data.pageCount){
-        html += '<li><a href="#">'+(pno+2)+'</a></li>';
-      }
-      $("#pagination").html(html);
-    },
-    error:function(){
-        alert("网络故障请检查！");
-    }
-})
+            //下下一页
+            if(pno+2<=data.pageCount){
+                html += '<li><a href="#">'+(pno+2)+'</a></li>';
+            }
+            $("#pagination").html(html);
+        },
+        error:function(){
+            alert("网络故障请检查！");
+        }
+    })
 };
 //选择添加船舶数据按钮
 $("#myselect").change(function(){
